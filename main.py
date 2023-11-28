5e6792e775fcc5b7928875c1ece49eb12ad4635d
Max-Frisch <max-frisch@gmx.net>
2023-11-28 18:34:34 +0700

    setting the title -.-

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
-
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Asteroid Shooter')
 
+# create a surface
+test_surf = pygame.Surface((400,100))
 while True:
-    # 1 get input -> events (mouse click, mouse movement, keypress on a button or controller or touchscreen)
+    # 1. get input -> events (mouse click, mouse movement, keypress on a button or controller or touchscreen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    # 2 updates
+            sys.exit()
 
-    # 3 show the frame to the player / update the display surface
+    # 2. updates
+
+    # 3. show the frame to the player / update the display surface
     pygame.display.update()
-    
